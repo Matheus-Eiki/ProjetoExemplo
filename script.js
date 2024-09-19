ce9c5486839a1eee22d2ed202654c277a7f18bbb
Matheus <rm559483@fiap.com.br>
2024-09-19 11:14:47 -0300

    criando as variaveis indefinidas e vazias

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // alert("Ola Mundo-externo")
 
 //DECLARANDO AS VARIAVEIS
+
+//declaração indefinida
+let nome3;
+console.log(nome3)
+
+//declaração vazia
+let disciplina =null;
+console.log(disciplina)
+
+//declaração definida ou com valor
 var nome="fiap";
 console.log(nome)
 
@@ -9,3 +19,4 @@ console.log(nome1)
 
 const nome2="javascript";
 console.log(nome2)
+
