6cc2ff47867ac130d24fbdb35874161809558292
Matheus <rm559483@fiap.com.br>
2024-09-19 11:23:09 -0300

    criando tipos de variaveis

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ console.log(nome1)
 const nome2="javascript";
 console.log(nome2)
 
+//TIPOS DE VARIÁVEIS
+
+//INTEIRO
+let idade = 18;
+console.log(idade)
+
+//FLUTUANTES OU COM CASAS DECIMAIS
+let valor = 6.40;
+console.log(valor)
+
+//STRINGS (TEXTOS) COM ASPAS DUPLAS
+let texto="Fiap";
+console.log(texto)
+
+//BOOLEANOS -VERDADEIRO OU FALSO
+
+let aluno=true
+console.log(aluno)
+
+//ARRAY - CONJUNTO DE INFORMAÇÕES
+let tecnologias =["JAVA","C#","HTML","CSS"];
+console.log(tecnologias)
